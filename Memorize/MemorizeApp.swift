c8af5421af02a72e36b16c1f9ddc4ef45811b6b3
Edwin George <edwgeo@gmail.com>
2022-08-16 22:54:14 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Edwin George on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
